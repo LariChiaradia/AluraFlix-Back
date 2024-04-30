9c0c684317f626e634aee5c01a48ed9b3ff93359
Larissa Chiaradia <lari.chiaradia.silva@gmail.com>
2024-04-29 22:04:47 -0300

    feat/Create Class Videos

---
+++ aluraFlix-back/Models/Videos.cs
@@ -0,0 +1,10 @@
+﻿namespace aluraFlix_back.Models
+{
+    public class Videos
+    {
+        public int VideoId { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string url { get; set; }
+    }
+}
